bcf0818037112976292bcc96fd73f7b5471cb275
leonbos1 <ktmleon@live.nl>
2021-04-13 13:08:21 +0200

    Update server
    inkomende data wordt opgesplitst in scanner en tag zodat deze later apart kunnen worden opgeslagen in de database

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 def main():
     HOST = "192.168.178.69" 
     PORT = 4000       
-    
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         print("Binding done, waiting for client")
@@ -13,11 +13,30 @@ def main():
         with connection:
             print("Connected by", addr)
             while True:
+                tag = ''
+                scanner = ''
+                scanner_bool = False
+                tag_bool = True
+
                 data = connection.recv(1024)
                 message = data.decode("utf-8")
                 if not data:
                     return
                 print("Received data: ", message)
+                for element in message:
+                    if tag_bool:
+                        tag += element
+                        if element == ' ':
+                            scanner_bool = True
+                            tag_bool = False
+                            
+                    if scanner_bool:
+                        scanner += element
+                scanner= scanner[1:]
+
+
+                print(tag)
+                print(scanner)
 
 if __name__ == '__main__':
     main()
