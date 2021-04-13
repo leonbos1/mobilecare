00bd7a04f54647da48af71f079e480e227120b43
leonbos1 <ktmleon@live.nl>
2021-04-13 10:19:29 +0200

    Update main.py
    verstuurt nu ook naar server welke scanner de tag scande

--- main.py
+++ main.py
@@ -24,31 +24,23 @@ def main():
     if uname()[0] == 'esp32':
         rdr1 = mfrc522.MFRC522(sck=18, mosi=23, miso=19, rst=0, cs=22)
         rdr2 = mfrc522.MFRC522(sck=18, mosi=23, miso=19, rst=0, cs=15)
-        print("Place card before reader to read")
-        print(rdr1)
-        print(rdr2)
 
     while True:
         (stat1, tag_type1) = rdr1.request(rdr1.REQIDL)
         (stat2, tag_type2) = rdr2.request(rdr2.REQIDL)
-        print(stat1)
-        print(stat2)
         if stat1 == rdr1.OK:
-            print(stat1)
             (stat1, uid) = rdr1.SelectTagSN()
             if stat1 == rdr1.OK:
                 print("Card detected at scanner #1 %s" % uidToString(uid))
-                tagdata = uidToString(uid)
-                data = {scanner:1,tag:tagdata}
-                s.send(bytes(str(data, "utf-8"))
+                data = str(uidToString(uid)) + ' Scanner_1'
+                s.send(bytes(data, "utf-8"))
 
         if stat2 == rdr2.OK:
             (stat2, uid) = rdr2.SelectTagSN()
             if stat2 == rdr2.OK:
                 print("Card detected at scanner #2 %s" % uidToString(uid))
-                tagdata = uidToString(uid)
-                data = {scanner:1,tag:tagdata}
-                s.send(bytes(str(data, "utf-8"))
+                data = str(uidToString(uid)) + ' Scanner_2'
+                s.send(bytes(data, "utf-8"))
 
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
