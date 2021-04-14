298b8acf3b80101a2e62b67c0ff001ac1f2f7c17
leonbos1 <ktmleon@live.nl>
2021-04-14 20:05:46 +0200

    update
    db model toegevoegd in comments

--- db_commands.py
+++ db_commands.py
@@ -6,3 +6,23 @@ c = conn.cursor()
 def addtosensordata(id, timeactivated, timedeactivated, tag, activation_duration):
     c.execute(f"insert into sensor_time(sensor_id,time_activated, time_deactivated, tag, activation_duration) values ({id},'{timeactivated}','{timedeactivated}','{tag}','{activation_duration}')")
     conn.commit()
+
+"""
+CREATE TABLE "sensor_time" (
+	"id"	INTEGER NOT NULL,
+	"sensor_id"	INTEGER NOT NULL,
+	"time_activated"	TEXT,
+	"time_deactivated"	TEXT,
+	"tag"	TEXT,
+	"activation_duration"	INTEGER,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "sensor" (
+	"id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+
+
+"""
