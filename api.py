6f8b6979352cc1d93abc0c95640a66f2337dbd75
Rene Vrieling <rene.vrieling@hotmail.com>
2021-05-17 12:08:21 +0200

    made a login and change a whole lot

--- api.py
+++ api.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, make_response, g, jsonify
+from functools import wraps
+
+import jwt
+from cerberus import Validator
+from flask import Flask, request, Response, g, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
 import re
-
+import bcrypt
+import string
+import random
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,7 +25,7 @@ db = SQLAlchemy(app)
 sensor = {}
 
 class SensorTime(db.Model): #door leon
-    id =  db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     sensor_id = db.Column(db.Integer)
     time_activated = db.Column(db.String)
     time_deactivated = db.Column(db.String)
@@ -29,15 +36,40 @@ class SensorTime(db.Model): #door leon
         return f"Sensor(id={self.id}, sensor_id={self.sensor_id}, time_activated={self.time_activated}, time_deactivated={self.time_deactivated}, tag={self.tag}, activation_duration={self.activation_duration})"
 
 
-class Verzorgers(db.Model): #door leon
+class Users(db.Model): #door leon
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String)
     firstname = db.Column(db.String)
     lastname = db.Column(db.String)
     email = db.Column(db.String)
     password = db.Column(db.String)
+    role = db.Column(db.String)
 
     def __repr__(self):
-        return f'Verzorger(id={self.id}, firstname={self.firstname}, lastname={self.lastname}, email={self.email}, password={self.password})'
+        return f'Gebruiker(id={self.id}, public_id={self.public_id}, firstname={self.firstname}, lastname={self.lastname}, email={self.email}, password={self.password}), role={self.role}'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+
+        token = None
+
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+
+        if not token:
+            abort(401)
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = Users.query.filter_by(public_id=data['public_id']).first()
+        except:
+            abort(401)
+
+        return f(*args, **kwargs)
+    return decorator
+
 
 #door leon
 sensor_put_args = reqparse.RequestParser()
@@ -48,15 +80,15 @@ sensor_put_args.add_argument("time_deactivated", type=str, help='dit is de tijd 
 sensor_put_args.add_argument("tag", type=str, help='dit is de tag die de sensor heeft gescand')
 sensor_put_args.add_argument("activation_duration", type=int, help='dit is de tijd in seconde dat de sensor is geactivate')
 
-verzorger_put_args = reqparse.RequestParser()
-verzorger_put_args.add_argument("firstname", type=str, help="dit is de voornaam van een verzorger")
-verzorger_put_args.add_argument("lastname", type=str, help='dit is de achternaam van een verzorger')
-verzorger_put_args.add_argument("email", type=str, help='dit is de email van een verzorger')
-verzorger_put_args.add_argument("password", type=str, help='dit is de password van een verzorger')
+user_post_args = reqparse.RequestParser()
+user_post_args.add_argument("firstname", type=str, help="dit is de voornaam van een gebruiker")
+user_post_args.add_argument("lastname", type=str, help='dit is de achternaam van een gebruiker')
+user_post_args.add_argument("email", type=str, help='dit is de email van een gebruiker')
+user_post_args.add_argument("password", type=str, help='dit is de password van een gebruiker')
 
-verzorger_login_args = reqparse.RequestParser()
-verzorger_login_args.add_argument("email", type=str, help='dit is de email van een verzorger')
-verzorger_login_args.add_argument("password", type=str, help='dit is de password van een verzorger')
+user_login_args = reqparse.RequestParser()
+user_login_args.add_argument("email", type=str, help='dit is de email van een gebruiker')
+user_login_args.add_argument("password", type=str, help='dit is de password van een gebruiker')
 
 #door leon
 sensor_data = {
@@ -68,17 +100,19 @@ sensor_data = {
     'activation_duration': fields.Integer
 }
 #door leon
-verzorger_data = {
-    'id' : fields.Integer,
-    'firstname' : fields.String,
-    'lastname' : fields.String,
-    'email' : fields.String,
-    'password' : fields.String
+user_data = {
+    'id': fields.Integer,
+    'public_id': fields.String,
+    'firstname': fields.String,
+    'lastname': fields.String,
+    'email': fields.String,
+    'password': fields.String,
+    'role': fields.String
 }
 #door leon
-verzorger_login = {
-    'email' : fields.String,
-    'password' : fields.String
+user_login = {
+    'email': fields.String,
+    'password': fields.String
 }
 #door leon
 class Sensor(Resource):
@@ -94,65 +128,104 @@ class Sensor(Resource):
         db.session.add(data)
         db.session.commit()
         return data, 201
+
+
 #door leon
-class Verzorger(Resource):
-    @marshal_with(verzorger_data)
+class User(Resource):
+    def __init__(self):
+        schema = {
+            'email': {'required': True, 'type': 'string'},
+            'firstname': {'required': True, 'type': 'string'},
+            'lastname': {'required': True, 'type': 'string'},
+            'password': {'required': True, 'type': 'string'},
+            'role': {'required': True, 'type': 'string'}
+        }
+        self.v = Validator(schema)
+
+    @marshal_with(user_data)
+    @token_required
     def get(self):
-        result = Verzorgers.query.all()
+        result = Users.query.all()
         return result
 
-    @marshal_with(verzorger_data)
-    def put(self):
-        args = verzorger_put_args.parse_args()
-        email = args['email']
-        email_result = Verzorgers.query.filter_by(email=email).first()
-        if email_result != None:
-            abort(401, message = 'Email is already taken')
-        if self.check_mail(email) != 'Valid':
-            abort(401, message = 'Invalid email')
+    def post(self):
+        args = request.get_json(force=True)
+        if self.v.validate(args):
+            email = args['email']
+            email_result = Users.query.filter_by(email=email).first()
+            if email_result != None:
+                abort(401, message = 'Email is already taken')
+            if self.check_mail(email) != 'Valid':
+                abort(401, message = 'Invalid email')
 
-        password = args['password']
-        if len(password) < 10:
-            abort(401, message = 'Password is too short')
-        weird_char = False
-        number = False
-        hoofdletter = False
-        for element in password:
-            if element in '~!@#$%^&*()_+=-,.<>/?;:"':
-                weird_char = True
-            if element in '0123456789':
-                number = True
-            if element in 'QWERTYUIOPASDFGHJKLZXCVBNM':
-                hoofdletter = True
-        if not weird_char or not number or not hoofdletter:
-            abort(401, message = 'Password does not meet security requirements')
-            
+            password = args['password']
+            if len(password) < 10:
+                abort(401, message = 'Password is too short')
+            weird_char = False
+            number = False
+            hoofdletter = False
+            for element in password:
+                if element in '~!@#$%^&*()_+=-,.<>/?;:"':
+                    weird_char = True
+                if element in '0123456789':
+                    number = True
+                if element in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+                    hoofdletter = True
+            if not weird_char or not number or not hoofdletter:
+                abort(401, message = 'Password does not meet security requirements')
 
-        data = Verzorgers(firstname=args['firstname'], lastname=args['lastname'], email=args['email'], password=generate_password_hash(args['password'], method='sha256'))
-        db.session.add(data)
-        db.session.commit()
-        return data, 201
+            data = Users(
+                public_id=self.id_generator(80),
+                firstname=args['firstname'],
+                lastname=args['lastname'],
+                email=args['email'],
+                password=bcrypt.hashpw(args['password'].encode('utf-8'), salt=bcrypt.gensalt()),
+                role=args['role']
+            )
+            db.session.add(data)
+            db.session.commit()
+            return Response(data, 201)
+        else:
+            return Response('missing fields', 400)
 
     def check_mail(self, email):
         if(re.search(regex, email)):
             return 'Valid'
         return 'Invalid'
 
+    def id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
 #door leon
-class VerzorgerLogin(Resource):
-    @marshal_with(verzorger_login)
+class UserLogin(Resource):
+    def __init__(self):
+        self.schema = {'email': {'required': True, 'type': 'string'}, 'password': {'required': True, 'type': 'string'}}
+        self.v = Validator(self.schema)
+
     def post(self):
-        args = verzorger_login_args.parse_args()
-        email = args['email']
-        password = args['password']
-        print(email, password)
+        args = request.get_json(force=True)
+
+        if self.v.validate(args):
+            email = args['email']
+            password = args['password']
+            user = Users.query.filter(Users.email == email).first()
+            if user:
+                if bcrypt.checkpw(password.encode('utf-8'), user.password):
+                    token = jwt.encode({'public_id': user.public_id}, app.config['SECRET_KEY'], algorithm='HS256')
+                    return jsonify({'token' : token})
+                else:
+                    print('test')
+            else:
+                return Response('invalid combination', status=400)
+        else:
+            return Response('missing fields', status=400)
     
-api.add_resource(Sensor, "/sensordata/")    
-api.add_resource(Verzorger, "/verzorgers/")
-api.add_resource(VerzorgerLogin, "/login/")
+api.add_resource(Sensor, "/sensordata")
+api.add_resource(User, "/users")
+api.add_resource(UserLogin, "/login")
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.178.69', port=80,debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
